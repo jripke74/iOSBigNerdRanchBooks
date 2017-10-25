63f897d671d4025a26d1f0ebe4197f71da7cc730
Jeff Ripke <jripke74@gmail.com>
2017-10-25 11:55:59 -0700

    initial commit

---
+++ Quiz/Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Jeff Ripke on 10/25/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quiz/Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Jeff Ripke on 10/25/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
