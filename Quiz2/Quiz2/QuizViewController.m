190c7228308e83bf64fff2bebe0d13264b8aa88b
Jeff Ripke <jripke74@gmail.com>
2017-10-31 10:41:43 -0700

    Quiz2: implemented initWithNibName

--- Quiz2/Quiz2/QuizViewController.m
+++ Quiz2/Quiz2/QuizViewController.m
@@ -10,12 +10,28 @@
 
 @interface QuizViewController ()
 
+@property (nonatomic) int currentQuestionIndex;
+
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
 @property (nonatomic, weak) IBOutlet UILabel *questionLabel;
 @property (nonatomic, weak) IBOutlet UILabel *answerLabel;
 
 @end
 
 @implementation QuizViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    // call the init method implemented by the superclass
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self) {
+        self.questions = @[@"From what is cognac made?", @"What is 7+7?", @"What is the capitol of Vermont?"];
+        self.answers = @[@"Grapes", @"14", @"Montpelier"];
+    }
+    return self;
+}
 
 - (IBAction)showQuestion:(id)sender {
     
