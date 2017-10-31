416d09f5837266911876cf00c4e6181e45e7ce08
Jeff Ripke <jripke74@gmail.com>
2017-10-31 11:18:03 -0700

    Quiz2: added array of questions and answer and logic to display them

--- Quiz2/Quiz2/QuizViewController.m
+++ Quiz2/Quiz2/QuizViewController.m
@@ -22,23 +22,37 @@
 
 @implementation QuizViewController
 
-- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    // call the init method implemented by the superclass
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    
+- (void)viewDidLoad {
+    [super viewDidLoad];
     if (self) {
         self.questions = @[@"From what is cognac made?", @"What is 7+7?", @"What is the capitol of Vermont?"];
         self.answers = @[@"Grapes", @"14", @"Montpelier"];
     }
-    return self;
 }
+//- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+//    // call the init method implemented by the superclass
+//    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+//    
+//    if (self) {
+//        self.questions = @[@"From what is cognac made?", @"What is 7+7?", @"What is the capitol of Vermont?"];
+//        self.answers = @[@"Grapes", @"14", @"Montpelier"];
+//    }
+//    return self;
+//}
 
 - (IBAction)showQuestion:(id)sender {
-    
+    self.currentQuestionIndex++;
+    if (self.currentQuestionIndex == [self.questions count]) {
+        self.currentQuestionIndex = 0;
+    }
+    NSString *question = self.questions[self.currentQuestionIndex];
+    self.questionLabel.text = question;
+    self.answerLabel.text = @"?..??";
 }
 
 - (IBAction)showAnswer:(id)sender {
-    
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    self.answerLabel.text = answer;
 }
 
 @end
