37da2cce043b5df37e0fab053d44f08d00b739f7
Jeff Ripke <jripke74@gmail.com>
2017-11-01 09:30:21 -0700

    RandomItem: first commit, add array

---
+++ RandomItems/RandomItems/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Jeff Ripke on 11/1/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+        [items addObject:@"One"];
+        [items addObject:@"Two"];
+        [items addObject:@"Three"];
+        [items insertObject:@"Zero" atIndex:0];
+        items = nil;
+    }
+    return 0;
+}
