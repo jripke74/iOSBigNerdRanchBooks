b03dd21e5a91ffe9659f8999375cbb8a22514535
Jeff Ripke <jripke74@gmail.com>
2017-11-16 10:18:13 -0800

    Hypnosister: add HypnosisView class

--- Hypnosister/Hypnosister/HypnosisView.h
+++ Hypnosister/Hypnosister/HypnosisView.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  HypnosisView.h
 //  Hypnosister
 //
-//  Created by Jeff Ripke on 11/14/17.
+//  Created by Jeff Ripke on 11/16/17.
 //  Copyright © 2017 Jeff Ripke. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface HypnosisView : UIView
 
 @end
-
---
+++ Hypnosister/Hypnosister/HypnosisView.m
@@ -0,0 +1,21 @@
+//
+//  HypnosisView.m
+//  Hypnosister
+//
+//  Created by Jeff Ripke on 11/16/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import "HypnosisView.h"
+
+@implementation HypnosisView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
