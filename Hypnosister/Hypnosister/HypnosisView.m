f191b4cd208ddcf9a4009017c6bafd6b68d2b81b
Jeff Ripke <jripke74@gmail.com>
2017-11-30 11:56:15 -0800

    removed line connecting concentric server

--- Hypnosister/Hypnosister/HypnosisView.m
+++ Hypnosister/Hypnosister/HypnosisView.m
@@ -28,6 +28,7 @@
     UIBezierPath *path = [[UIBezierPath alloc] init];
     //[path addArcWithCenter:center radius:radius startAngle:0.0 endAngle:M_PI * 2.0 clockwise:YES];
     for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
+        [path moveToPoint:CGPointMake(center.x + currentRadius, center.y)];
         [path addArcWithCenter:center radius:currentRadius startAngle:0.0 endAngle:M_PI * 2.0 clockwise:YES];
     }
     path.lineWidth = 10.0;
